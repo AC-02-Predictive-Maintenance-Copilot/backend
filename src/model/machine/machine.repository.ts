cc2e19a69be1dd23b7d477a104eabe0c8575f5a8
AbdulAzis666 <lingkaran001@gmail.com>
2025-11-09 19:12:20 +0700

    Menambah metod Get by id

--- src/model/machine/machine.repository.ts
+++ src/model/machine/machine.repository.ts
@@ -3,4 +3,9 @@ import { TMachineInput } from './machine.validator';
 
 export const findAllMachines = async () => await prisma.machine.findMany({});
 
+export const findMachineById = async (id: string) =>
+  await prisma.machine.findUnique({
+    where: { id },
+  });
+
 export const createMachine = async (data: TMachineInput) => await prisma.machine.create({ data });
