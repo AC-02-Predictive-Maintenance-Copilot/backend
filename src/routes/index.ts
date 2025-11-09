1d592ff429479c0a65c973cd7399f8300cf77064
AbdulAzis666 <lingkaran001@gmail.com>
2025-11-09 15:41:09 +0700

    perbaikan bug

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import machineRouter from '../model/machine/machine.route.js';
+import machineRouter from '../model/machine/machine.route';
 
 const router = Router();
 
