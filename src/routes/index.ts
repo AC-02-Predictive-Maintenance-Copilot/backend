90b27b4a21cd93dbd9accdd584191f70cc61edf3
zachriek <zachrie2005@gmail.com>
2025-12-04 11:47:35 +0700

    feat: add manage users route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import authRouter from '../model/auth/auth.route';
 import machineRouter from '../model/machine/machine.route';
 import ticketRouter from '../model/ticket/ticket.route';
 import messageRouter from '../model/message/message.route';
+import userRouter from '../model/user/user.route';
 import { requireAuth } from '../middleware/auth';
 
 const router = Router();
@@ -17,5 +18,6 @@ router.use(requireAuth);
 router.use('/machines', machineRouter);
 router.use('/tickets', ticketRouter);
 router.use('/chat', messageRouter);
+router.use('/users', userRouter);
 
 export { router };
