2a4b94150d2d55ea05072a05ba465ea5be2f6895
zachriek <zachrie2005@gmail.com>
2025-12-04 11:53:00 +0700

    feat: update user select in user.repository

--- dist/src/model/user/user.repository.js
+++ dist/src/model/user/user.repository.js
@@ -6,10 +6,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteUserById = exports.unverifyUser = exports.verifyUser = exports.findUserById = exports.findAllUsers = void 0;
 const client_1 = require("@prisma/client");
 const prisma_1 = __importDefault(require("../../lib/prisma"));
-const findAllUsers = async () => await prisma_1.default.user.findMany({ where: { role: client_1.ERole.ENGINEER } });
+const userSelect = {
+    id: true,
+    name: true,
+    username: true,
+    email: true,
+    isVerified: true,
+    role: true,
+    picture: true,
+    createdAt: true,
+    updatedAt: true,
+};
+const findAllUsers = async () => await prisma_1.default.user.findMany({ where: { role: client_1.ERole.ENGINEER }, select: userSelect });
 exports.findAllUsers = findAllUsers;
 const findUserById = async (userId) => await prisma_1.default.user.findUnique({
     where: { id: userId },
+    select: userSelect,
 });
 exports.findUserById = findUserById;
 const verifyUser = async (userId) => {
@@ -18,6 +30,7 @@ const verifyUser = async (userId) => {
         data: {
             isVerified: true,
         },
+        select: userSelect,
     });
 };
 exports.verifyUser = verifyUser;
@@ -27,10 +40,12 @@ const unverifyUser = async (userId) => {
         data: {
             isVerified: false,
         },
+        select: userSelect,
     });
 };
 exports.unverifyUser = unverifyUser;
 const deleteUserById = async (userId) => await prisma_1.default.user.delete({
     where: { id: userId },
+    select: userSelect,
 });
 exports.deleteUserById = deleteUserById;
