560d63a5bbcc1a24272ca68385b9a04e04e88e92
zachriek <zachrie2005@gmail.com>
2025-12-05 20:53:57 +0700

    fix: param validation ticket

--- src/model/ticket/ticket.route.ts
+++ src/model/ticket/ticket.route.ts
@@ -2,14 +2,14 @@ import { Router } from 'express';
 import { validateBody, validateParams } from '../../middleware/validate';
 import { ticketSchema } from './ticket.validator';
 import { createTicketHandler, getTicketHandler, updateTicketHandler, deleteTicketHandler, getTicketByIdHandler } from './ticket.controller';
-import { paramsIdSchema } from '../../validator';
+import { paramsIdTicketSchema } from '../../validator';
 
 const router = Router();
 
 router.get('/', getTicketHandler);
-router.get('/:id', validateParams(paramsIdSchema), getTicketByIdHandler);
+router.get('/:id', validateParams(paramsIdTicketSchema), getTicketByIdHandler);
 router.post('/', validateBody(ticketSchema), createTicketHandler);
-router.put('/:id', validateParams(paramsIdSchema), updateTicketHandler);
-router.delete('/:id', validateParams(paramsIdSchema), deleteTicketHandler);
+router.put('/:id', validateParams(paramsIdTicketSchema), updateTicketHandler);
+router.delete('/:id', validateParams(paramsIdTicketSchema), deleteTicketHandler);
 
 export default router;
